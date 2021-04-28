7265015ffa8dc79cac0db06bb5c85b98043cbe6a
Alan Rajendram <alan.rajendram@gmail.com>
2021-04-28 08:06:28 +0200

    Refactoring the code

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,6 @@ const menuIcon = document.querySelector('.hamburger-menu');
 const navbar = document.querySelector('.sidenav');
 
 menuIcon.addEventListener('click', () => {
-  if (navbar.classList.toggle('active')) {
-    navbar.style.width = '300px';
-  } else {
-    navbar.style.width = '0';
-  }
+  menuIcon.classList.toggle('hamburger-menu--active')
+  navbar.classList.toggle('sidenav--active')
 })
